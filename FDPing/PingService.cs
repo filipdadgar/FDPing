96c563b6515da02a25aabdc4ccf80c8b727a73d8
filipdadgar <filip@dadgar.se>
2024-03-14 13:52:44 +0100

    Histogram works

--- FDPing/PingService.cs
+++ FDPing/PingService.cs
@@ -19,31 +19,24 @@ public class PingService : BackgroundService
 {
     private readonly ILogger<PingService> _logger;
     private readonly IConfiguration _configuration;
-    private readonly Counter<long> _successCount;
-    private readonly Counter<long> _failureCount;
-    private readonly ActivitySource tracer = new("PingService");
-    private readonly MeterProvider _meterProvider;
-    // private readonly TracerProvider _tracerProvider;
+    private static readonly ActivitySource tracer = new ActivitySource("PingService");
+    private static MeterProvider _meterProvider;
+    
+    private static string _serviceName = "PingService";
+    private static readonly Meter _successCount = new ("ping.success");
+    private static readonly Meter _failureCount = new ("ping.failure");
+    private static readonly Meter _roundTripTime = new ("ping.roundtripTime");
 
-
-    public PingService(IConfiguration configuration, ILogger<PingService> logger, IMeterFactory meterFactory)
+    public PingService(IConfiguration configuration, ILogger<PingService> logger)
     {
-        var SuccessMeter = meterFactory.Create(new MeterOptions("ping.success"));
-        var SuccessFailure = meterFactory.Create(new MeterOptions("ping.failure"));
-        _successCount = SuccessMeter.CreateCounter<long>("success_count");
-        _failureCount = SuccessFailure.CreateCounter<long>("failure_count");
-
-
-
         _configuration = configuration;
         _logger = logger;
-
         _meterProvider = Sdk.CreateMeterProviderBuilder()
-            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("PingService"))
-            .AddMeter("ping.success")
-            .AddMeter("ping.failure")
-            .AddPrometheusHttpListener(
-                options => options.UriPrefixes = new string[] { "http://localhost:9464/" })
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(_serviceName))
+            .AddMeter(_successCount.Name)
+            .AddMeter(_failureCount.Name)
+            .AddView(instrumentName: "RoundTripTime", new ExplicitBucketHistogramConfiguration())
+            .AddPrometheusHttpListener()
             // Add the Prometheus exporter
             .AddPrometheusExporter()
             .AddOtlpExporter(opt =>
@@ -55,21 +48,6 @@ public class PingService : BackgroundService
             })
             //.AddConsoleExporter()
             .Build();
-
-        // _tracerProvider = Sdk.CreateTracerProviderBuilder()
-        //     .AddSource("PingService")
-        //     .AddProcessor(new CustomConsoleProcessor())
-        //     //.AddConsoleExporter()
-        //     .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("PingService"))
-        //     .AddOtlpExporter(opt =>
-        //     {
-        //         opt.Protocol = OtlpExportProtocol.Grpc;
-        //         // Get the endpoint from the configuration
-        //         var endpoint = _configuration.GetSection("otlp:Endpoint").Value;
-        //         opt.Endpoint = new Uri(endpoint);
-        //         
-        //     })
-        //     .Build();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -104,15 +82,20 @@ public class PingService : BackgroundService
                 {
                     activity?.SetTag("status", "success");
                     activity?.SetTag("roundtripTime", reply.RoundtripTime);
+                    var histogram = _successCount.CreateHistogram<long>("Roundtrip_time");
+                    // Record the round trip time with unit of milliseconds
+                    histogram.Record(reply.RoundtripTime, new TagList { { "host", host } });
+                    
                     // Increment the success counter for this host
-                    _successCount.Add(1, new TagList { { "host", host } });
+                    _successCount.CreateCounter<long>("success_count").Add(1, new TagList { { "host", host } });
                     _logger.LogInformation($"Ping to {host} successful. Round Trip Time: {reply.RoundtripTime}ms");
+                    
                 }
                 else
                 {
                     activity?.SetTag("status", "failure");
                     // Increment the failure counter for this host
-                    _failureCount.Add(1, new TagList { { "host", host } });
+                    _failureCount.CreateCounter<long>("failure_count").Add(1, new TagList { { "host", host } });
                     _logger.LogInformation($"Ping to {host} failed.");
                 }
             }
@@ -120,7 +103,7 @@ public class PingService : BackgroundService
             {
                 activity?.SetTag("status", "failure");
                 // Increment the failure counter for this host
-                _failureCount.Add(1, new TagList { { "host", host } });
+                _failureCount.CreateCounter<long>("failure_count").Add(1, new TagList { { "host", host } });
                 _logger.LogInformation($"Ping to {host} failed with exception: {ex.Message}");
             }
         }
